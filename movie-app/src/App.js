05bed98fa4a3cbe7c6a85f020833f1df76711a30
xcapde <xcapde@hotmail.com>
2022-05-10 09:42:00 +0200

    react working

---
+++ movie-app/src/App.js
@@ -0,0 +1,32 @@
+import './styles/main.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <body>
+
+<div class="movies_list">
+    
+    <div class="movie_card">
+        <div class="movie_img">
+            <img src="https://es.web.img2.acsta.net/medias/nmedia/18/90/04/41/20078157.jpg" alt="movie cover"/>
+            <p class="img_year">2022</p>
+        </div>
+        <div class="movie_info">
+            <div class="card_text">
+                <h1>Spider-man No Way Home</h1>
+                <h2>Acció, Superherois, Aventures, Familiar</h2>
+            </div>
+            <button class="card_button"><i class="fa-solid fa-star"></i></button>
+        </div>
+    </div>
+
+</div>
+
+</body>
+    </div>    
+  );
+}
+
+export default App;
---
+++ movie-app/src/components/MovieList.jsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class MovieList extends Component{
+
+    render() {
+
+        return <h1>HELLOOOO it's meeeee</h1>;
+    }
+}
