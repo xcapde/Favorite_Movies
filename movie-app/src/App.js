d28808bbbf589b924517831838d40f45dcc6bf68
xcapde <xcapde@hotmail.com>
2022-05-10 12:47:21 +0200

    cards i dades render ok

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -9,26 +9,9 @@ function App() {
       < MovieList />
       
       <body>
-      
-      <div class="movies_list">
-          
-          <div class="movie_card">
-              <div class="movie_img">
-                  <img src="https://es.web.img2.acsta.net/medias/nmedia/18/90/04/41/20078157.jpg" alt="movie cover"/>
-                  <p class="img_year">2022</p>
-              </div>
-              <div class="movie_info">
-                  <div class="card_text">
-                      <h1>Spider-man No Way Home</h1>
-                      <h2>Acció, Superherois, Aventures, Familiar</h2>
-                  </div>
-                  <button class="card_button"><i class="fa-solid fa-star"></i></button>
-              </div>
-          </div>
+        
+      </body>
 
-      </div>
-
-</body>
     </div>    
   );
 }
