6166ac3a8e1ccd4b9135782f783ddd176dbde61c
xcapde <xcapde@hotmail.com>
2022-05-26 11:06:42 +0200

    functional preview component

--- movie-app/src/components/Movies/form/preview/Preview.jsx
+++ movie-app/src/components/Movies/form/preview/Preview.jsx
@@ -1,22 +1,13 @@
-import { Component } from "react";
 import '../preview/preview.css';
 
+export function Preview (props){
 
-export class Preview extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            seePreview:this.props.seePreview,
-            newMovie:this.props.newMovie,
-        }
-    };
+    const newMovie = props.newMovie;
 
-    render(){
-        return  (
-                <div className="previewMovie">
-                        <img src={this.props.newMovie.imgURL} alt="Preview"/>
-                        <div className="previewMovie_detail">Preview</div>
-                </div>                
-                )
-        };
+    return  (
+            <div className="previewMovie">
+                    <img src={newMovie.imgURL} alt="Preview"/>
+                    <div className="previewMovie_detail">Preview</div>
+            </div>                
+            )
 }
